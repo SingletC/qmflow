4def020aa57dc3d93426f47001b4fbc28c8ec334
Chong <chong.teng@bc.edu>
2023-02-03 09:26:01 -0500

    add lifetime

--- web/dashboard/pages/data.py
+++ web/dashboard/pages/data.py
@@ -24,6 +24,8 @@ def create_dataframe(db: ase.db.core.Database, selection=None, columns=None):
         table +=[row]
     columns.append('Structure')
     df = pd.DataFrame(table,columns=columns)
-
+    df['lifetime(ns)'] = round(
+        1.5/((1e7 / df['lambda_'])**2 * df['osc_str']) *1e9 # ns
+        , 2)
     df['ctime'] = df['ctime'].apply(lambda x: datetime.fromtimestamp(x * ase.db.core.YEAR + ase.db.core.T2000))#.strftime('%c'))
     return df
