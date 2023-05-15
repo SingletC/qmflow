aca57656179fb360cd630dfa8983ad3625062543
Chong <chong.teng@bc.edu>
2023-05-15 09:12:28 -0400

    molpro

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='qmflow',
+    version='',
+    packages=['web', 'web.dashboard', 'web.dashboard.pages', 'calc', 'flow'],
+    url='',
+    license='',
+    author='singletc',
+    author_email='',
+    description=''
+)
