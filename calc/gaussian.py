1bae9a6a7050c39d6f28f215278b2bad21529629
Chong <chong.teng@bc.edu>
2023-05-22 11:02:43 -0400

    a

---
+++ calc/gaussian.py
@@ -0,0 +1,39 @@
+from ase import Atoms
+from ase.calculators.calculator import FileIOCalculator
+from ase.calculators.gaussian import Gaussian
+from ase.constraints import FixAtoms
+from ase.io import write, read
+
+
+class Gaussian_fix(Gaussian):
+    command = 'GAUSSIAN PREFIX.com'  # avoid redirecting output, buffer may fail
+
+    def write_input(self, atoms, properties=None, system_changes=None):
+
+        FileIOCalculator.write_input(self, atoms, properties, system_changes)
+        write(self.label + '.com', atoms, properties=properties,
+              format='gaussian-in', parallel=False, **self.parameters)
+        # read .com file
+        if atoms.constraints:
+            if atoms.constraints[0].__class__.__name__ == 'FixAtoms':
+                with open(self.label + '.com', 'r') as f:
+                    # remove tag line
+                    lines = f.readlines()
+                    flag = 0
+                    for idx, line in enumerate(lines):
+                        if len(line.split()) ==4:
+                            flag +=1
+                        elif line =="\n" and flag > 1 and idx > 4:
+                            for i in (atoms.constraints[0].get_indices()):
+                                lines.insert(idx +1 , f"X {i+1} F\n")
+                            break
+                # write .com file back
+                with open(self.label + '.com', 'w') as f:
+                    f.writelines(lines)
+
+if __name__ == "__main__":
+    # Test
+    atoms = Atoms('H2O', [[0, 0, 0], [0, 0, 0.74], [0, 0, 1.64]])
+    atoms.constraints = [FixAtoms(indices=[0])]
+    atoms.calc = Gaussian_fix(method='opt=modredundant')
+    atoms.get_potential_energy()
