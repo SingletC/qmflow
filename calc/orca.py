ce48f6df450d7d81775134061b4da1bea8c6ba6e
Chong <chong.teng@bc.edu>
2023-05-24 09:38:30 -0400

    orca neb

--- calc/orca.py
+++ calc/orca.py
@@ -6,26 +6,32 @@ from ase.io import read
 
 class OrcaNEB:
     batch = """#!/bin/bash
-ulimit -s unlimited
-module purge
-module load orca
-export job=orca
-export RSH_COMMAND="/usr/bin/ssh -x"
-export scratchlocation=/scratch/
-tdir=$(mktemp -d $scratchlocation/$USER/orcajob__$SLURM_JOB_ID-XXXX)
-cp  $SLURM_SUBMIT_DIR/*.inp $tdir/
+    #SBATCH -t 100:00:00
+    #SBATCH --mem 240G
+    #SBATCH -p full_nodes64,full_nodes48,partial_nodes
+    #SBATCH -n 48
+    #SBATCH -N 1
+    ulimit -s unlimited
+    module purge
+    module load orca
+    export job=orca
+    export RSH_COMMAND="/usr/bin/ssh -x"
+    export scratchlocation=/dev/shm/
+    tdir=$(mktemp -d $scratchlocation//orcajob__$SLURM_JOB_ID-XXXX)
+    cp  $SLURM_SUBMIT_DIR/*.inp $tdir/
 
-cp  $SLURM_SUBMIT_DIR/*.gbw $tdir/
+    cp  $SLURM_SUBMIT_DIR/*.gbw $tdir/
 
-cp  $SLURM_SUBMIT_DIR/*.xyz $tdir/
-cd $tdir
-/usr/public/orca/orca_5_0_1_linux_x86-64_shared_openmpi411///orca $job.inp >  $SLURM_SUBMIT_DIR/$job.out
-cp $tdir/* $SLURM_SUBMIT_DIR
-"""
+    cp  $SLURM_SUBMIT_DIR/*.xyz $tdir/
+    cd $tdir
+    /usr/public/orca/orca_5_0_1_linux_x86-64_shared_openmpi411///orca $job.inp > $job.out
+    cp $tdir/* $SLURM_SUBMIT_DIR
+    rm $tdir/ -rf
+    """
 
     def __init__(self, method, label='orca_temp', srun_command=None):
         self.inp = f"""!{method} NEB-TS
-%PAL NPROCS 64 END
+%PAL NPROCS 48 END
 %NEB NEB_END_XYZFILE "init.xyz"
 preopt true
 NImages 8
@@ -33,7 +39,7 @@ END
 * XYZfile 0 1 final.xyz
 """
         self.label = label
-        self.srun = srun_command or os.getenv('ORCA_CMD') or 'srun -n 64 ./orca.sh'
+        self.srun = srun_command or os.getenv('ORCA_CMD') or 'sbatch --wait orca.sh'
 
     def run_neb(self, r: Atoms, p: Atoms):
         # make directory if not exist
@@ -42,10 +48,9 @@ END
         # write input file
         with open(f'{self.label}/orca.inp', 'w') as f:
             f.write(self.inp)
-        # write bash file and make executable
+        # write bash file
         with open(f'{self.label}/orca.sh', 'w') as f:
             f.write(self.batch)
-        os.system(f'chmod +x {self.label}/orca.sh')
         # write initial and final xyz files
         r.write(f'{self.label}/init.xyz')
         p.write(f'{self.label}/final.xyz')
