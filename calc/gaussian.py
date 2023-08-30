8d79105cf8d6753c309e6229e7c5744b7781a240
Chong <chong.teng@bc.edu>
2023-08-30 09:51:50 -0400

    add FixInternals for ase gaussian

--- calc/gaussian.py
+++ calc/gaussian.py
@@ -1,7 +1,7 @@
 from ase import Atoms
 from ase.calculators.calculator import FileIOCalculator
 from ase.calculators.gaussian import Gaussian
-from ase.constraints import FixAtoms
+from ase.constraints import FixAtoms, FixInternals
 from ase.io import write, read
 
 
@@ -18,24 +18,36 @@ class Gaussian_fix(Gaussian):
               format='gaussian-in', parallel=False, **self.parameters)
         # read .com file
         if atoms.constraints:
-            if atoms.constraints[0].__class__.__name__ == 'FixAtoms':
+            inserted_lines = []
+            for constraint in atoms.constraints:
+                if constraint.__class__.__name__ == 'FixAtoms':
+                    for i in (constraint.get_indices()):
+                        inserted_lines += [f"X {i + 1} F\n"]
+                if constraint.__class__.__name__ == 'FixInternals':
+                    constraint: FixInternals
+                    for _, i in constraint.dihedrals:
+                        inserted_lines += [f"D {i[0] + 1} {i[1] + 1} {i[2] + 1} {i[3] + 1} F\n"]
+
+                # write .com file back
                 with open(self.label + '.com', 'r') as f:
                     # remove tag line
                     lines = f.readlines()
                     flag = 0
                     for idx, line in enumerate(lines):
-                        if len(line.split()) ==4:
-                            flag +=1
-                        elif line =="\n" and flag > 1 and idx > 4:
-                            for i in (atoms.constraints[0].get_indices()):
-                                lines.insert(idx +1 , f"X {i+1} F\n")
+                        if len(line.split()) == 4:
+                            flag += 1
+                        elif line == "\n" and flag > 1 and idx > 4:
+                            for inserted_line in inserted_lines:
+                                lines.insert(idx + 1, inserted_line)
                             break
-                # write .com file back
                 with open(self.label + '.com', 'w') as f:
                     f.writelines(lines)
+
+
 if __name__ == "__main__":
     atoms = Atoms('H2O', [[0, 0, 0], [0, 0, 0.74], [0, 0, 1.64]])
     atoms.constraints = [FixAtoms(indices=[0])]
+    atoms.constraints = [FixInternals(dihedrals_deg=[[0, [0, 1, 2, 3]], [1, [1, 2, 3, 4]]])]
     atoms.calc = Gaussian_fix(method='opt=modredundant')
     atoms.get_potential_energy()
 
