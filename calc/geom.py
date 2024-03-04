00c55287fe5137fed7b3cc05016994d2d9536f35
Chong <chong.teng@bc.edu>
2024-03-04 12:07:36 -0500

    add rmsd tool

---
+++ calc/geom.py
@@ -0,0 +1,17 @@
+from ase.atoms import Atoms
+import rmsd
+
+
+def calc_rmsd(atoms1: Atoms, atoms2: Atoms) -> float:
+    """
+    calculate the rmsd between two atoms, after remove rotation and translation
+    Args:
+        atoms1:
+        atoms2:
+
+    Returns:
+
+    """
+    x1 = atoms1.get_positions()
+    x2 = atoms2.get_positions()
+    rmsd.kabsch_rmsd(x1, x2, translate=True)
